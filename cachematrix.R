31a5b609721cc5941a58f14a9d3c8e46bb6b0171
pmdzns <phil@pmdzns.com>
2014-06-21 16:31:38 -0400

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Compute the inverse of a square matrix and cach the result
+## makeCacheMatrix creates the cacheable matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix creates the cacheable matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+ m <- NULL # set cache flag
+ 
+        set <- function(y) {
+                x <<- y    # assign to gobal env
+                m <<- NULL # assign to gobal env or cache
+        }
+        get <- function() x # function to retrieve gobal 
 
+        setsolve <- function(solve) m <<- solve # function to place value in global env
+
+        getsolve <- function() m # get inverse matrix from gobal env
+
+        list (set=set,get=get,setsolve=setsolve,getsolve=getsolve)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the inverse matrix. If not already in cache it solves for 'x' and caches the result
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+ m <- x$getsolve() #get the inverse matrix from the gobal env
+        if (!is.null(m)) { # if it (does not) = Null, grab the inverse matrix from cache
+                 message("getting cached data")
+                 return(m)
+        }
+        mdata <- x$get() # if not solved then solve and put the inverse matrix into cache
+        m <- solve(mdata)
+        x$setsolve(m)
+        m
+
 }
